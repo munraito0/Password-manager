d0fbabd7ccb49e433a7b094270f0d66371579afc
munraito1 <munraito.sora@gmail.com>
2026-03-21 07:11:35 +0300

    Доделал мейн

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
-
+from app.routers import users,folders,ciphers
+from app.database import engine, Base
 
 app = FastAPI()
 
-app.include_router()
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+app.include_router(users.router)
+app.include_router(folders.router)
+app.include_router(ciphers.router)
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("app.main:app", reload=True)
